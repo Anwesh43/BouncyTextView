fa3a2d24166c9a555ce484b9143f49bb4dcb69ee
Anwesh43 <anweshthecool0@gmail.com>
2019-11-11 10:20:40 +0530

    adding libraries and constants

--- bouncytextview/src/main/java/com/anwesh/uiprojects/bouncytextview/BouncyTextView.kt
+++ bouncytextview/src/main/java/com/anwesh/uiprojects/bouncytextview/BouncyTextView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.bouncytextview
 /**
  * Created by anweshmishra on 11/11/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val text : String = "HELLO"
+val scGap : Float = 0.02f
+val sizeFactor : Float = 2.9f
+val delay : Long = 30
+val foreColor : Int = Color.parseColor("#BDBDBD")
+val backColor : Int = Color.parseColor("#212121")
+
